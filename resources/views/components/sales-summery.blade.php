43ee31370d032eea51b0941a59fb37184735e362
H. M. Masum Billah <mbillah21@gmail.com>
2023-12-17 21:13:00 +0600

    Updated

--- resources/views/components/sales-summery.blade.php
+++ resources/views/components/sales-summery.blade.php
@@ -3,7 +3,7 @@
 )
 
 <div class="flex-1 mx-2">
-    <div class="bg-white max-w-sm rounded overflow-hidden shadow-lg">
+    <div class="bg-white max-w-sm rounded overflow-hidden shadow-sm">
         <div class="px-6 py-4">
           <div class="font-bold text-xl mb-2">{{$title}}</div>
           <hr>
